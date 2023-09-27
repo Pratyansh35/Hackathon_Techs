d06a3f00beda49c35a95d98d88a2db821674eccd
Pratyansh Maddhesia <pratyansh35@gmail.com>
2023-09-27 20:55:00 +0530

    "Agency Reg"

--- Frontend/src/components/Java script/Agency.js
+++ Frontend/src/components/Java script/Agency.js
@@ -1,32 +1,52 @@
 import React from 'react'
-import '../css/Agency.css'
+import "../css/Agency.css"
+
+var sg = document.getElementById("SignUpDiv");
+var lg = document.getElementById("loginDiv");
 export default function Agency() {
+    // function signUp() {
+    //     sg.style.visibility = "visible";
+    //     lg.style.visibility = "hidden";
+    // }
+    
+    function loginpg() {
+        sg.style.visibility = "hidden";
+        lg.style.visibility = "visible";
+    }
     return (
         <div>
-            <form action="action_page.php" style={{border:"1px solid #ccc"}}>
-                <div class="container">
-                    <h1>Sign Up</h1>
+            
+            <form action="action_page.php">
+                <div class="loginDiv" id='loginDiv'>
+                    falfaf
+                </div>
+
+
+
+                <div class="SignUpDiv" id='SignUpDiv'>
+                    <h1>Registration</h1>
                     <p>Please fill in this form to create an account.</p>
-                    <hr></hr>
+                    <hr/>
 
                     <label for="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" required></input>
+                    <input type="text" placeholder="Enter Email" name="email" required/>
 
                     <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required></input>
+                    <input type="password" placeholder="Enter Password" name="psw" required/>
 
                     <label for="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
+                    <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
 
                     <label>
-                        <input type="checkbox" checked="checked" name="remember" style={{ margin: "15px" }}> Remember me</input>
+                        <input type="checkbox" checked="checked" name="remember" style={{ margin: "15px" } }/> Remember me
                     </label>
-
-                    <p>By creating an account you agree to our <a href="#" style={{ color: "dodgerblue" }}>Terms & Privacy</a>.</p>
-
                     <div class="clearfix">
-                        <button type="button" class="cancelbtn">Cancel</button>
-                        <button type="submit" class="signupbtn">Sign Up</button>
+                    <p>By creating an account you agree to our <a href="#" style={{ color: "dodgerblue" }}>Terms & Privacy</a>.</p>
+                    <p style={{float:"right"}}>Already Registered <a onClick={loginpg} style={{ color: "dodgerblue" }}>Login</a>.</p>
+                    </div>
+                    <div class="clearfix">
+                        <button id="cancelbtn" type="button" class="cancelbtn">Cancel</button>
+                        <button id="signUpbtn"type="submit" class="signupbtn">Sign Up</button>
                     </div>
                 </div>
                 </form> 
