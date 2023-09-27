cdbd743cca92a94068c79200cb654c6835bf7e8b
nickychy <96295935+nickychy@users.noreply.github.com>
2023-09-27 15:31:21 +0530

    Update Video.js

--- Frontend/src/components/Java script/Video.js
+++ Frontend/src/components/Java script/Video.js
@@ -8,54 +8,82 @@ export default function Video() {
     {
       
       embedUrl: 'https://www.youtube.com/embed/im4DI6UNzIA?si=IDD3zdXv5XGOTo_e" title="YouTube video player',
-      title: 'floods',
+      title: 'flood',
     },
     {
-      title: 'earthquakhjvbe',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
+      title: 'earthquake',
+      embedUrl: 'https://www.youtube.com/embed/BLEPakj1YTY?si=J4Rt67rnRc4e2Pt_',
+    },
+    {
+      title: 'flood',
+      embedUrl: 'https://www.youtube.com/embed/SgScj6-NzQg?si=nWiY7_rSpJQtFsNf',
+
+    },
+    {
+      title: 'drought',
+      embedUrl: 'https://www.youtube.com/embed/RrmWXfiM_b8?si=30THTqejfXWER-PG',
 
     },
     {
       title: 'earthquake',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
+      embedUrl: 'https://www.youtube.com/embed/r5EbbrVXoQw?si=SG51e54kZ9tVE3Pm',
+
+    },
+    {
+      title: 'Tsunami',
+      embedUrl: 'https://www.youtube.com/embed/7EDflnGzjTY?si=OQcCqLiXS8DMcscQ',
 
     },
     {
       title: 'drought',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
+      embedUrl: 'https://www.youtube.com/embed/oXG5SaZCVNU?si=DlzDQALrhSbbD9aM',
 
     },
     {
       title: 'earthquake',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
+      embedUrl: 'https://www.youtube.com/embed/RqqqSnaTfQo?si=uvEei3ks1eg_EG8c',
 
+    },
+    {
+      title: 'tsunami',
+      embedUrl: 'https://www.youtube.com/embed/fQAciMgl-kM?si=TaqOfvqBtooPwNio',
+
+    },
+    {
+      title: 'flood',
+      embedUrl: 'https://www.youtube.com/embed/HYIXcvshlYo?si=X-dl8vn6DHa6tu2A',
+
+    },
+    {
+      
+      embedUrl: 'https://www.youtube.com/embed/im4DI6UNzIA?si=IDD3zdXv5XGOTo_e" title="YouTube video player',
+      title: 'flood',
+    },
+    {
+      title: 'earthquake',
+      embedUrl: 'https://www.youtube.com/embed/BLEPakj1YTY?si=J4Rt67rnRc4e2Pt_',
+    },
+
+    {
+      title: 'tsunami',
+      embedUrl: 'https://www.youtube.com/embed/P1HX9Uh8OUg?si=1NerDz8WWi4q34Er',
+
+    },
+    {
+      title: 'flood',
+      embedUrl: 'https://www.youtube.com/embed/LiZ5hryi2Sw?si=fW6JVG3F6_25UUXH',
+     
     },
     {
       title: 'drought',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
-
-    },
-    {
-      title: 'drought',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
-
-    },
-    {
-      title: 'floods',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
-
-    },
-    {
-      title: 'floods',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
-
-    },
-    {
-      title: 'floods',
-      embedUrl: 'https://www.youtube.com/embed/https://www.youtube.com/watch?v=TkFz4sySV7M&list=RDTkFz4sySV7M&start_radio=1',
+      embedUrl: 'https://www.youtube.com/embed/5qMl5UDQkZc?si=bwnYRlbpMztd4kvc',
 
     },
    
+    {
+      title: 'drought',
+      embedUrl: 'https://www.youtube.com/embed/gmz8sFN2CeA?si=ZWJ73eV_7WUSRvSY',
+    },
   ]);
 
   const handleInputChange = (event) => {
