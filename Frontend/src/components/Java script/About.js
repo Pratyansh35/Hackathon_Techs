e535a6e6c56fe4bf8d536226672af947ddf2ef0b
Pratyansh Maddhesia <pratyansh35@gmail.com>
2023-09-28 12:22:33 +0530

    asfdas

--- Frontend/src/components/Java script/About.js
+++ Frontend/src/components/Java script/About.js
@@ -1,10 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
-export default function About() {
+import "../css/About.css";
+
+const About = () => {
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  useEffect(() => {
+    showSlides(slideIndex);
+  }, [slideIndex]);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    setSlideIndex((slideIndex) => slideIndex + n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    setSlideIndex(n);
+  }
+
+  function showSlides(n) {
+    let slides = document.querySelectorAll(".mySlides");
+    let dots = document.querySelectorAll(".dot");
+
+    if (n > slides.length) {
+      setSlideIndex(1);
+    }
+
+    if (n < 1) {
+      setSlideIndex(slides.length);
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
   return (
     <>
-      <Header/>
-      
+      <Header />
+      <div className="slideshow-container">
+        <div className="mySlides fade">
+          <div className="numbertext">1 / 3</div>
+          <img src="../../img1.jpg" style={{ width: "100%" }} />
+          <div className="text">Caption Text</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">2 / 3</div>
+          <img src="../../img2.jpg" style={{ width: "100%" }} />
+          <div className="text">Caption Two</div>
+        </div>
+
+        <div className="mySlides fade">
+          <div className="numbertext">3 / 3</div>
+          <img src="../../img3.jpg" style={{ width: "100%" }} />
+          <div className="text">Caption Three</div>
+        </div>
+
+        <a className="prev" onClick={() => plusSlides(-1)}>
+          &#10094;
+        </a>
+        <a className="next" onClick={() => plusSlides(1)}>
+          &#10095;
+        </a>
+      </div>
+      <br />
+
+      <div style={{ textAlign: "center" }}>
+        <span className="dot" onClick={() => currentSlide(1)}></span>
+        <span className="dot" onClick={() => currentSlide(2)}></span>
+        <span className="dot" onClick={() => currentSlide(3)}></span>
+      </div>
     </>
   );
-}
+};
+
+export default About;
